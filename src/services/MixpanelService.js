fd0bdd0b5c4a1de8edb0e08de88f570361ebbb68
Vuk Nikolic <vuknikolic@gmail.com>
2018-10-12 13:17:46 +0200

    #7 Refactoring, all push notification code is in one method now.

--- src/services/MixpanelService.js
+++ src/services/MixpanelService.js
@@ -1,30 +1,30 @@
 import { LOCAL_CONFIG } from '../../config'
 import {Platform, PushNotificationIOS} from 'react-native'
-
 const Mixpanel = require('react-native-mixpanel')
 
 export const MixpanelService = {
   init: async function (userId, email) {
-    if (Platform.OS === 'ios') {
-      PushNotificationIOS.requestPermissions().then(function (response) {
-        PushNotificationIOS.addEventListener('register', function(token){
-          mixpanelInstance.addPushDeviceToken(token)
-        });
-      })
-    }
-
     let mixpanelInstance = Mixpanel.default
     try {
       await mixpanelInstance.sharedInstanceWithToken(LOCAL_CONFIG.MIXPANEL.PROJECT_TOKEN)
       await mixpanelInstance.identify(userId)
       await mixpanelInstance.set({"$email": email});
-      if (Platform.OS === 'android') {
-        await mixpanelInstance.initPushHandling(LOCAL_CONFIG.MIXPANEL.GOOGLE_SENDER_ID)
-      }
+      await this.setPushNotificationToken(mixpanelInstance)
       await mixpanelInstance.track("Mobile app started")
     } catch (e) {
       console.log('MixPanel error', e)
     }
-
-  }
+  },
+  setPushNotificationToken: async function (mixpanelInstance) {
+    if (Platform.OS === 'android') {
+      await mixpanelInstance.initPushHandling(LOCAL_CONFIG.MIXPANEL.GOOGLE_SENDER_ID)
+    }
+    if (Platform.OS === 'ios') {
+      PushNotificationIOS.requestPermissions().then(function (response) {
+        PushNotificationIOS.addEventListener('register', function (token) {
+          mixpanelInstance.addPushDeviceToken(token)
+        })
+      })
+    }
+  },
 }
