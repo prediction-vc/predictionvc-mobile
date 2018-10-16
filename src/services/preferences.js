2e90aeb5a22f198f603e5bb285481c08ce7aba77
Vuk Nikolic <vuknikolic@gmail.com>
2018-10-16 14:58:52 +0200

    #7 Formatting.

--- src/services/preferences.js
+++ src/services/preferences.js
@@ -1,111 +1,111 @@
-import { AsyncStorage } from 'react-native';
-import { Base64 } from 'js-base64';
+import { AsyncStorage } from 'react-native'
+import { Base64 } from 'js-base64'
 
-const STORAGE_KEY_ENCRYPTED = 'preferences';
+const STORAGE_KEY_ENCRYPTED = 'preferences'
 
-async function loadRawPreferences() {
+async function loadRawPreferences () {
   try {
-    const enc = await AsyncStorage.getItem(STORAGE_KEY_ENCRYPTED);
+    const enc = await AsyncStorage.getItem(STORAGE_KEY_ENCRYPTED)
     if (enc) {
-      const json = Base64.decode(enc);
-      return JSON.parse(json);
+      const json = Base64.decode(enc)
+      return JSON.parse(json)
     }
   } catch (error) {
-    console.log(JSON.stringify(error));
+    console.log(JSON.stringify(error))
   }
-  return null;
+  return null
 }
 
-async function saveRawPreferences(preferences) {
+async function saveRawPreferences (preferences) {
   try {
-    const json = JSON.stringify(preferences);
-    const raw = Base64.encode(json);
-    await AsyncStorage.setItem(STORAGE_KEY_ENCRYPTED, raw);
+    const json = JSON.stringify(preferences)
+    const raw = Base64.encode(json)
+    await AsyncStorage.setItem(STORAGE_KEY_ENCRYPTED, raw)
   } catch (error) {
-    console.log(JSON.stringify(error));
+    console.log(JSON.stringify(error))
   }
 }
 
 class Preference {
-  preferences: undefined;
+  preferences: undefined
 
-  async init() {
+  async init () {
     try {
-      this.preferences = await loadRawPreferences();
+      this.preferences = await loadRawPreferences()
       if (!this.preferences) {
-        await saveRawPreferences({'testing':'yup'})
+        await saveRawPreferences({'testing': 'yup'})
       }
     } catch (e) {
       console.log('Preferences.init error', e)
     }
   };
 
-  async getAll() {
-    return this.preferences;
+  async getAll () {
+    return this.preferences
   };
 
-  async getItem(key) {
+  async getItem (key) {
     try {
-      if (!this.preferences){
-        await this.init();
+      if (!this.preferences) {
+        await this.init()
       }
-      return this.preferences[key];
+      return this.preferences[key]
     } catch (e) {
       return {}
     }
 
   };
 
-  async setItem(key, value) {
-    if (!this.preferences){
-      await this.init();
+  async setItem (key, value) {
+    if (!this.preferences) {
+      await this.init()
     }
-    this.preferences[key] = value;
+    this.preferences[key] = value
     await saveRawPreferences(this.preferences)
   };
 
-  async removeItem(key) {
-    if (!this.preferences){
-      await this.init();
+  async removeItem (key) {
+    if (!this.preferences) {
+      await this.init()
     }
-    delete this.preferences[key];
+    delete this.preferences[key]
     await saveRawPreferences(this.preferences)
   };
 
-  async removeMultiItem(keys) {
-    if (!this.preferences){
-      await this.init();
+  async removeMultiItem (keys) {
+    if (!this.preferences) {
+      await this.init()
     }
-    keys.map(key => delete this.preferences[key]);
+    keys.map(key => delete this.preferences[key])
     await saveRawPreferences(this.preferences)
   };
 
-  async parseJsonItem(key) {
-    const json = await this.getItem(key);
+  async parseJsonItem (key) {
+    const json = await this.getItem(key)
     if (json) {
-      return JSON.parse(json);
+      return JSON.parse(json)
     }
-    return null;
+    return null
   };
 
-  async saveItemAsJson(key, value) {
+  async saveItemAsJson (key, value) {
     if (value) {
-      return this.setItem(key, JSON.stringify(value));
+      return this.setItem(key, JSON.stringify(value))
     }
-    return this.removeItem(key);
+    return this.removeItem(key)
   };
 
-  async clear() {
-    await AsyncStorage.clear();
+  async clear () {
+    await AsyncStorage.clear()
   }
 
 }
 
-export const Preferences = new Preference();
+export const Preferences = new Preference()
 
-export const JWT_TOKEN = 'jwt';
-export const USER_PROFILE_INFO  = 'USER_PROFILE_INFO';
-export const PROJECT_TOKEN_INFO = 'PROJECT_TOKEN_INFO';
-export const USER_TOKEN_NEWS    = 'USER_TOKEN_NEWS';
-export const COMPLETE_TUTORIAL  = 'COMPLETE_TUTORIAL';
-export const DISPLAY_COLUMNS    = 'DISPLAY_COLUMNS';
+export const JWT_TOKEN = 'jwt'
+export const USER_PROFILE_INFO = 'USER_PROFILE_INFO'
+export const PROJECT_TOKEN_INFO = 'PROJECT_TOKEN_INFO'
+export const USER_TOKEN_NEWS = 'USER_TOKEN_NEWS'
+export const COMPLETE_TUTORIAL = 'COMPLETE_TUTORIAL'
+export const DISPLAY_COLUMNS = 'DISPLAY_COLUMNS'
